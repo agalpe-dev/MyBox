c1b2a2707fedff0766d44f193cd72d9a983d4b81
Antonio Gálvez <agalpe.dev@gmail.com>
2021-05-20 15:17:31 +0200

    Pequeños arreglos diseño Ajustes y funcionalidad gestión archivos

--- app/src/main/java/com/agp/mybox/UI/Ajustes/FragmentAjustesViewModel.java
+++ app/src/main/java/com/agp/mybox/UI/Ajustes/FragmentAjustesViewModel.java
@@ -3,13 +3,23 @@ package com.agp.mybox.UI.Ajustes;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.ViewModel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 
 public class FragmentAjustesViewModel extends AndroidViewModel {
-
+    private Handler handler=new Handler();
     private final String PREFERENCIAS="Preferencias";
     private SharedPreferences mPrefs = getApplication().getSharedPreferences(PREFERENCIAS, Context.MODE_PRIVATE);
 
@@ -28,5 +38,86 @@ public class FragmentAjustesViewModel extends AndroidViewModel {
         SharedPreferences.Editor editor =mPrefs.edit();
         editor.putBoolean(pref,valor);
         editor.commit();
+    }
+
+    public String getNArchivos(String carpeta){
+        File[] files=getFilesFromDir(carpeta);
+        if (files!=null){
+            return Integer.toString(files.length) + " archivo(s)" ;
+        }
+        return "Sin archivos";
+    }
+
+    public File[] getFilesFromDir(String carpeta){
+        File [] files;
+        File f = new File(getApplication().getFilesDir(),"box/"+carpeta);
+        if (f.exists() && f.isDirectory()){
+            files=f.listFiles();
+            return files;
+        }
+        return null;
+    }
+
+    public String getFilesSize (String carpeta){
+        File [] files=getFilesFromDir(carpeta);
+        AssetFileDescriptor fileDescriptor= null;
+        Uri uri=null;
+        float size=0;
+        DecimalFormat decimalFormat=new DecimalFormat("#.##");
+        if (files.length>0) {
+            try {
+                for (File f : files) {
+                    uri = FileProvider.getUriForFile(getApplication(), "com.agp.mybox.fileprovider", f);
+                    fileDescriptor = getApplication().getContentResolver().openAssetFileDescriptor(uri, "r");
+                    size+=fileDescriptor.getLength();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return decimalFormat.format(size/(1024*1024))+" Mb";
+        }
+        return "0 Mb";
+    }
+
+    public void borrarRecursos(String carpeta) {
+        if (carpeta=="todo"){
+            borrarTodosRecursos();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                File[] files=getFilesFromDir(carpeta);
+                if (files!=null){
+                    for (File f: files){
+                        try {
+                            f.delete();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                // Para acceder al hilo principal y poner el Toast
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplication(), "¡Archivos de \"" + carpeta.toUpperCase() +"\" eliminados correctamente!", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+            }
+        }).start();
+
+    }
+
+    public void borrarTodosRecursos(){
+        borrarRecursos("camara");
+        borrarRecursos("imagen");
+        borrarRecursos("pdf");
+        borrarRecursos("otros");
     }
 }
--- app/src/main/java/com/agp/mybox/UI/Ajustes/fragment_ajustes.java
+++ app/src/main/java/com/agp/mybox/UI/Ajustes/fragment_ajustes.java
@@ -1,6 +1,7 @@
 package com.agp.mybox.UI.Ajustes;
 
-import android.os.Build;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,22 +9,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.agp.mybox.R;
-import com.agp.mybox.UI.Inicio.FragmentInicioViewModel;
-
-import java.util.Observer;
 
 public class fragment_ajustes extends Fragment {
 
     private FragmentAjustesViewModel mViewModel;
     private Switch swAvisos, swOCR;
+    private TextView mNCamara, mNImagen, mNPdf, mNOtros, mTamCamara, mTamImagen, mTamPdf, mTamOtros;
+    private ImageButton mBtBorrarCamara, mBtBorrarImagen, mBtBorrarPdf, mBtBorrarOtros;
+    private Button mBtBorrarTodo;
 
     public static fragment_ajustes newInstance() {
         return new fragment_ajustes();
@@ -36,8 +39,59 @@ public class fragment_ajustes extends Fragment {
         final View v=inflater.inflate(R.layout.fragment_ajustes,container,false);
         mViewModel= new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())).get(FragmentAjustesViewModel.class);
 
+        // Setear controles
         swAvisos= v.findViewById(R.id.botonAvisos);
         swOCR= v.findViewById(R.id.botonOCR);
+        mNCamara=v.findViewById(R.id.txtNCamara);
+        mNImagen =v.findViewById(R.id.txtNImagen);
+        mNPdf=v.findViewById(R.id.txtNPdf);
+        mNOtros=v.findViewById(R.id.txtNOtros);
+        mTamCamara=v.findViewById(R.id.txtTamCamara);
+        mTamImagen =v.findViewById(R.id.txtTamImagen);
+        mTamPdf=v.findViewById(R.id.txtTamPdf);
+        mTamOtros=v.findViewById(R.id.txtTamOtros);
+
+        mBtBorrarCamara=v.findViewById(R.id.btBorrarCamara);
+        mBtBorrarImagen =v.findViewById(R.id.btBorrarImagen);
+        mBtBorrarPdf=v.findViewById(R.id.btBorrarPdf);
+        mBtBorrarOtros=v.findViewById(R.id.btBorrarOtros);
+        mBtBorrarTodo=v.findViewById(R.id.btBorrarTodo);
+
+        // Listeners de botones para borrar
+        mBtBorrarCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAviso("camara");
+            }
+        });
+
+        mBtBorrarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAviso("imagen");
+            }
+        });
+
+        mBtBorrarPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAviso("pdf");
+            }
+        });
+
+        mBtBorrarOtros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAviso("otros");
+            }
+        });
+
+        mBtBorrarTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAviso("todo");
+            }
+        });
 
         // Establecer valores de los switchbutton según lo almacenado en SharedPreferences
         swAvisos.setChecked(mViewModel.checkPreferencias("Avisos"));
@@ -57,6 +111,19 @@ public class fragment_ajustes extends Fragment {
             }
         });
 
+
+        // Datos de archivos y tamaños por carpetas
+        mNCamara.setText(mViewModel.getNArchivos("camara"));
+        mNImagen.setText(mViewModel.getNArchivos("imagen"));
+        mNPdf.setText(mViewModel.getNArchivos("pdf"));
+        mNOtros.setText(mViewModel.getNArchivos("otros"));
+
+        mTamCamara.setText(mViewModel.getFilesSize("camara"));
+        mTamImagen.setText(mViewModel.getFilesSize("imagen"));
+        mTamPdf.setText(mViewModel.getFilesSize("pdf"));
+        mTamOtros.setText(mViewModel.getFilesSize("otros"));
+
+
         // Se devuelve la vista
         return v;
     }
@@ -67,6 +134,30 @@ public class fragment_ajustes extends Fragment {
 
     }
 
+
+    private void crearAviso(String carpeta){
+        AlertDialog.Builder dialog= new AlertDialog.Builder(getContext());
+        dialog.setTitle(R.string.borrarArchivos);
+        dialog.setMessage(R.string.avisoBorrarRecursos);
+        dialog.setPositiveButton(R.string.borrar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (carpeta!=null) {
+                    mViewModel.borrarRecursos(carpeta);
+                    // Toast.makeText(getActivity(), "Archivos eliminados", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getActivity(), "Hay un problema y no se pueden eliminar los archivos en este momento.\n Vuelve a intentarlo.", Toast.LENGTH_LONG).show();
+                }
+            }
+        })
+                .setNegativeButton(R.string.boton_cancelar, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                }).show();
+
+    }
 
 
 }
